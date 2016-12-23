f06423df34028464f2e2d30d5e52545326569006
Junu Gurung <junugurung48@gmail.com>
2016-12-23 09:55:46 -0500

    jquery traversing used

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
-$("#q1").click(function(){
-	$("#a1").fadeToggle("slow");
-	$("#arrow-down1").toggleClass("collapse");
-	$("#arrow-up1").toggleClass("collapse");
+$(".question").click(function(){
+	$(this).next().fadeToggle("slow");
+	console.log($(this).children().toggleClass("collapse"));
+	// $("#a1").fadeToggle("slow");
+	// $("#arrow-down1").toggleClass("collapse");
+	// $("#arrow-up1").toggleClass("collapse");
+	
 	});
 
-$("#q2").click(function(){
-	$("#a2").fadeToggle("slow");
-	$("#arrow-down2").toggleClass("collapse");
-	$("#arrow-up2").toggleClass("collapse");
+// $("#q2").click(function(){
+// 	$("#a2").fadeToggle("slow");
+// 	$("#arrow-down2").toggleClass("collapse");
+// 	$("#arrow-up2").toggleClass("collapse");
 
-});
+// });
 
-$("#q3").click(function(){
-	$("#a3").fadeToggle("slow");
-	$("#arrow-down3").toggleClass("collapse");
-	$("#arrow-up3").toggleClass("collapse");
+// $("#q3").click(function(){
+// 	$("#a3").fadeToggle("slow");
+// 	$("#arrow-down3").toggleClass("collapse");
+// 	$("#arrow-up3").toggleClass("collapse");
 
-});
+// });
+
+// $("#q4").click(function(){
+// 	$("#a4").fadeToggle("slow");
+// 	$("#arrow-down4").toggleClass("collapse");
+// 	$("#arrow-up4").toggleClass("collapse");
+
+// });
