f6993ae996df533597d47b3c434df9b6191edf57
Junu Gurung <junugurung48@gmail.com>
2016-12-23 08:54:44 -0500

    style page added with css modified

---
+++ index.js
@@ -0,0 +1,19 @@
+$("#q1").click(function(){
+	$("#a1").fadeToggle("slow");
+	$("#arrow-down1").toggleClass("collapse");
+	$("#arrow-up1").toggleClass("collapse");
+	});
+
+$("#q2").click(function(){
+	$("#a2").fadeToggle("slow");
+	$("#arrow-down2").toggleClass("collapse");
+	$("#arrow-up2").toggleClass("collapse");
+
+});
+
+$("#q3").click(function(){
+	$("#a3").fadeToggle("slow");
+	$("#arrow-down3").toggleClass("collapse");
+	$("#arrow-up3").toggleClass("collapse");
+
+});
